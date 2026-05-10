4b49da6ab33e575e13bcead456bf70ebca18196b
Alisher <alexkarabayev@gmail.com>
2026-05-10 20:15:48 +0500

    Setup database config

---
+++ app/database.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_async_engine(DATABASE_URL,echo = True)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
